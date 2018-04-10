2aea98e3a71bbccbe79395a7dbd136f02118625b
Unknown <imre.nagy@ratoong.com>
2018-04-10 10:44:51 +0200

    Added short version for document, and removed UNIT

--- init.sql
+++ init.sql
@@ -1,41 +1,8 @@
--- -----------------------------------------------------------
--- Entity Designer DDL Script for MySQL Server 4.1 and higher
--- -----------------------------------------------------------
--- Date Created: 04/08/2018 14:50:29
-
--- Generated from EDMX file: C:\Users\alexa\Source\Repos\ConsoleApp2\ClassLibrary1\Model1.edmx
--- Target version: 3.0.0.0
-
--- --------------------------------------------------
-
 DROP DATABASE IF EXISTS databasesys;
 CREATE DATABASE databasesys;
 USE databasesys;
 
--- --------------------------------------------------
--- Dropping existing FOREIGN KEY constraints
--- NOTE: if the constraint does not exist, an ignorable error will be reported.
--- --------------------------------------------------
-
-
---    ALTER TABLE `IngredientsUsedSet` DROP CONSTRAINT `FK_IngredientIngredientsUsed`;
-
---    ALTER TABLE `IngredientsUsedSet` DROP CONSTRAINT `FK_RecipeIngredientsUsed`;
-
---    ALTER TABLE `CategorySet` DROP CONSTRAINT `FK_RecipeCategory`;
-
---    ALTER TABLE `RecipesSubmittedSet` DROP CONSTRAINT `FK_UserRecipesSubmitted`;
-
---    ALTER TABLE `RatingSet` DROP CONSTRAINT `FK_RatingUser`;
-
---    ALTER TABLE `RatingSet` DROP CONSTRAINT `FK_RatingRecipe`;
-
---    ALTER TABLE `RecipesSubmittedSet` DROP CONSTRAINT `FK_RecipesSubmittedRecipe`;
-
-
--- --------------------------------------------------
 -- Dropping existing tables
--- --------------------------------------------------
 SET foreign_key_checks = 0;
 
     DROP TABLE IF EXISTS `Recipe`;
@@ -56,11 +23,7 @@ SET foreign_key_checks = 0;
 
 SET foreign_key_checks = 1;
 
--- --------------------------------------------------
 -- Creating all tables
--- --------------------------------------------------
-
-
 CREATE TABLE `Recipe`(
 	`RecipeId` int NOT NULL AUTO_INCREMENT UNIQUE, 
 	`Name` varchar(255) NOT NULL, 
@@ -69,20 +32,12 @@ CREATE TABLE `Recipe`(
 
 ALTER TABLE `Recipe` ADD PRIMARY KEY (`RecipeId`);
 
-
-
-
-
 CREATE TABLE `IngredientsUsed`(
 	`Amount` int NOT NULL, 
 	`IngredientId` int NOT NULL, 
 	`RecipeId` int NOT NULL);
 
 ALTER TABLE `IngredientsUsed` ADD PRIMARY KEY (`RecipeId`, `IngredientId`);
-
-
-
-
 
 CREATE TABLE `Ingredient`(
 	`IngredientId` int NOT NULL AUTO_INCREMENT UNIQUE, 
@@ -92,19 +47,11 @@ CREATE TABLE `Ingredient`(
 
 ALTER TABLE `Ingredient` ADD PRIMARY KEY (`IngredientId`);
 
-
-
-
-
 CREATE TABLE `RecipeCategory`(
 	`CategoryId` int NOT NULL, 
 	`RecipeId` int NOT NULL);
 
 ALTER TABLE `RecipeCategory` ADD PRIMARY KEY (`CategoryId`, `RecipeId`);
-
-
-
-
 
 CREATE TABLE `User`(
 	`UserId` int NOT NULL AUTO_INCREMENT UNIQUE, 
@@ -114,19 +61,11 @@ CREATE TABLE `User`(
 
 ALTER TABLE `User` ADD PRIMARY KEY (`UserId`);
 
-
-
-
-
 CREATE TABLE `RecipesSubmitted`(
 	`UserId` int NOT NULL, 
 	`RecipeId` int NOT NULL);
 
 ALTER TABLE `RecipesSubmitted` ADD PRIMARY KEY (`UserId`, `RecipeId`);
-
-
-
-
 
 CREATE TABLE `Rating`(
 	`RatingNum` int NOT NULL, 
@@ -135,10 +74,6 @@ CREATE TABLE `Rating`(
 
 ALTER TABLE `Rating` ADD PRIMARY KEY (`UserId`, `RecipeId`);
 
-
-
-
-
 CREATE TABLE `Category`(
 	`CategoryId` int NOT NULL AUTO_INCREMENT UNIQUE, 
 	`CategoryName` varchar(255) NOT NULL);
@@ -146,18 +81,7 @@ CREATE TABLE `Category`(
 ALTER TABLE `Category` ADD PRIMARY KEY (`CategoryId`,`CategoryName`);
 
 
-
-
-
-
-
--- --------------------------------------------------
 -- Creating all FOREIGN KEY constraints
--- --------------------------------------------------
-
-
--- Creating foreign key on `IngredientId` in table 'IngredientsUsed'
-
 ALTER TABLE `IngredientsUsed`
 ADD CONSTRAINT `FK_IngredientIngredientsUsed`
     FOREIGN KEY (`IngredientId`)
@@ -165,16 +89,9 @@ ADD CONSTRAINT `FK_IngredientIngredientsUsed`
         (`IngredientId`)
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-
--- Creating non-clustered index for FOREIGN KEY 'FK_IngredientIngredientsUsed'
-
 CREATE INDEX `IX_FK_IngredientIngredientsUsed`
     ON `IngredientsUsed`
     (`IngredientId`);
-
-
-
--- Creating foreign key on `RecipeId` in table 'IngredientsUsed'
 
 ALTER TABLE `IngredientsUsed`
 ADD CONSTRAINT `FK_RecipeIngredientsUsed`
@@ -183,20 +100,12 @@ ADD CONSTRAINT `FK_RecipeIngredientsUsed`
         (`RecipeId`)
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-
-
--- Creating foreign key on `UserId` in table 'RecipesSubmitted'
-
 ALTER TABLE `RecipesSubmitted`
 ADD CONSTRAINT `FK_UserRecipesSubmitted`
     FOREIGN KEY (`UserId`)
     REFERENCES `User`
         (`UserId`)
     ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-
-
--- Creating foreign key on `UserId` in table 'Rating'
 
 ALTER TABLE `Rating`
 ADD CONSTRAINT `FK_RatingUser`
@@ -205,10 +114,6 @@ ADD CONSTRAINT `FK_RatingUser`
         (`UserId`)
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-
-
--- Creating foreign key on `RecipeId` in table 'Rating'
-
 ALTER TABLE `Rating`
 ADD CONSTRAINT `FK_RatingRecipe`
     FOREIGN KEY (`RecipeId`)
@@ -216,16 +121,9 @@ ADD CONSTRAINT `FK_RatingRecipe`
         (`RecipeId`)
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-
--- Creating non-clustered index for FOREIGN KEY 'FK_RatingRecipe'
-
 CREATE INDEX `IX_FK_RatingRecipe`
     ON `Rating`
     (`RecipeId`);
-
-
-
--- Creating foreign key on `RecipeId` in table 'RecipesSubmitted'
 
 ALTER TABLE `RecipesSubmitted`
 ADD CONSTRAINT `FK_RecipesSubmittedRecipe`
@@ -234,16 +132,9 @@ ADD CONSTRAINT `FK_RecipesSubmittedRecipe`
         (`RecipeId`)
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-
--- Creating non-clustered index for FOREIGN KEY 'FK_RecipesSubmittedRecipe'
-
 CREATE INDEX `IX_FK_RecipesSubmittedRecipe`
     ON `RecipesSubmitted`
     (`RecipeId`);
-
-
-
--- Creating foreign key on `CategoryId` in table 'RecipeCategory'
 
 ALTER TABLE `RecipeCategory`
 ADD CONSTRAINT `FK_CategoryRecipeCategory`
@@ -252,10 +143,6 @@ ADD CONSTRAINT `FK_CategoryRecipeCategory`
         (`CategoryId`)
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-
-
--- Creating foreign key on `RecipeId` in table 'RecipeCategory'
-
 ALTER TABLE `RecipeCategory`
 ADD CONSTRAINT `FK_RecipeCategoryRecipe`
     FOREIGN KEY (`RecipeId`)
@@ -263,15 +150,6 @@ ADD CONSTRAINT `FK_RecipeCategoryRecipe`
         (`RecipeId`)
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-
--- Creating non-clustered index for FOREIGN KEY 'FK_RecipeCategoryRecipe'
-
 CREATE INDEX `IX_FK_RecipeCategoryRecipe`
     ON `RecipeCategory`
     (`RecipeId`);
-
-
-
--- --------------------------------------------------
--- Script has ended
--- --------------------------------------------------
