b225079c6f67f1b4a89f9c23667a2287460933c9
Vulthil <Alexander@atomic.dk>
2018-04-16 21:26:28 +0200

    Procedure backs up Ingredients aswell, and Event every month

--- Backup Procedure.sql
+++ Backup Procedure.sql
@@ -3,8 +3,10 @@ USE databasesys;
 
 -- CLEANUP
 DROP TABLE IF EXISTS RecipeOld;
+DROP TABLE IF EXISTS IngredientsOld;
+DROP TABLE IF EXISTS IngredientsUsedOld;
 DROP PROCEDURE IF EXISTS RecipeBackup;
-
+DROP EVENT IF EXISTS BackupEvent; 
 
 -- Backup Procedure example
 
@@ -16,6 +18,21 @@ ALTER TABLE RecipeOld
 	DROP INDEX RecipeId,
 	ADD PRIMARY KEY(RecipeId, BackupTime),
 	ADD UNIQUE (RecipeId, BackupTime);
+	
+CREATE TABLE IngredientsOld LIKE Ingredient;
+ALTER TABLE IngredientsOld 
+	ADD BackupTime TIMESTAMP(6),
+	DROP PRIMARY KEY,
+	DROP INDEX IngredientId,
+	ADD PRIMARY KEY(IngredientId, BackupTime),
+	ADD UNIQUE (IngredientId, BackupTime);
+	
+CREATE TABLE IngredientsUsedOld LIKE IngredientsUsed;
+ALTER TABLE IngredientsUsedOld 
+	ADD BackupTime TIMESTAMP(6),
+	DROP PRIMARY KEY,
+	ADD PRIMARY KEY(RecipeId, IngredientId, BackupTime),
+	ADD UNIQUE (RecipeId,IngredientId, BackupTime);
 	
 	
 -- Creating backup procedure for RecipeTable 
@@ -29,7 +46,9 @@ BEGIN
 			vSQLSTATE = RETURNED_SQLSTATE;
 		END;
 	START TRANSACTION;
-	INSERT INTO RecipeOld SELECT *, NOW(6) FROM Recipe;
+	INSERT INTO RecipeOld SELECT *,NOW(6) FROM Recipe;
+	INSERT INTO IngredientsOld SELECT *,NOW(6) FROM Ingredient;
+	INSERT INTO IngredientsUsedOld SELECT *,NOW(6) FROM IngredientsUsed;
 	IF vSQLSTATE = '00000' THEN COMMIT;
 		ELSE ROLLBACK;
 	END IF;
@@ -45,3 +64,9 @@ CALL RecipeBackup;
 -- Data after backup.
 SELECT * FROM Recipe;
 SELECT * FROM RecipeOld;
+
+
+CREATE EVENT BackupEvent
+ON SCHEDULE EVERY 1 MONTH
+STARTS '2018-05-01 00:00:00'
+DO CALL RecipeBackup();
